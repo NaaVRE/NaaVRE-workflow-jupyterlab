6b3263d429673c580aaa7c95e3b3e67b197b68ce
Gabriel Pelouze <gabriel@pelouze.net>
2025-01-07 15:46:47 +0100

    remove react-window dependency
    react-window is used to render the list of cells from the catalogue, but
    types definitions are broken with react 18. The number of cells is small
    enough that we can remove react-window for the moment. If the number of
    cells grows in the future, we can re-implement infinite-scroll or add
    pagination.

--- src/components/VirtualizedList.tsx
+++ src/components/VirtualizedList.tsx
@@ -3,35 +3,6 @@ import Box from '@mui/material/Box';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
-import { FixedSizeList, ListChildComponentProps } from 'react-window';
-
-function renderRow(
-  props: ListChildComponentProps,
-  clickAction: (cell_index: number) => void
-) {
-  const { data, index, style } = props;
-
-  return (
-    <ListItem style={style} key={index} component="div" disablePadding>
-      <ListItemButton
-        onClick={() => {
-          clickAction(index);
-        }}
-      >
-        <ListItemText
-          primary={data[index]['title']}
-          primaryTypographyProps={{
-            style: {
-              whiteSpace: 'nowrap',
-              overflow: 'hidden',
-              textOverflow: 'ellipsis'
-            }
-          }}
-        />
-      </ListItemButton>
-    </ListItem>
-  );
-}
 
 interface IVirtualizedListProps {
   clickAction: (index: number) => void;
@@ -48,16 +19,28 @@ export function VirtualizedList({ items, clickAction }: IVirtualizedListProps) {
         bgcolor: 'background.paper'
       }}
     >
-      <FixedSizeList
-        itemData={items}
-        height={400}
-        width={360}
-        itemSize={50}
-        itemCount={items.length}
-        overscanCount={5}
-      >
-        {props => renderRow({ ...props }, (clickAction = clickAction))}
-      </FixedSizeList>
+      {items.map((item, index) => {
+        return (
+          <ListItem key={index} component="div" disablePadding>
+            <ListItemButton
+              onClick={() => {
+                clickAction(index);
+              }}
+            >
+              <ListItemText
+                primary={item['title']}
+                primaryTypographyProps={{
+                  style: {
+                    whiteSpace: 'nowrap',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis'
+                  }
+                }}
+              />
+            </ListItemButton>
+          </ListItem>
+        );
+      })}
     </Box>
   );
 }
