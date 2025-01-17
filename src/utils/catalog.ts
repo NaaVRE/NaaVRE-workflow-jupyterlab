6554b0d1b57d2fd7602b0fef802d91c978fff27d
Gabriel Pelouze <gabriel@pelouze.net>
2025-01-17 17:48:47 +0100

    replace mock for ${catalogueServiceUrl}/workflow-cells/

--- src/utils/catalog.ts
+++ src/utils/catalog.ts
@@ -1,15 +1,18 @@
-import { MockNaaVREExternalService } from '../naavre-common/mockHandler';
+import { NaaVREExternalService } from '../naavre-common/handler';
 import { ICell } from '../naavre-common/types/NaaVRECatalogue/WorkflowCells';
 
 export async function getCellsFromCatalogue(
   catalogueServiceUrl: string
 ): Promise<Array<ICell>> {
-  const resp = await MockNaaVREExternalService(
+  const resp = await NaaVREExternalService(
     'GET',
-    `${catalogueServiceUrl}/workflow-cells/`
+    `${catalogueServiceUrl}/workflow-cells/`,
+    {
+      accept: 'application/json'
+    }
   );
   if (resp.status_code !== 200) {
     throw `${resp.status_code} ${resp.reason}`;
   }
-  return JSON.parse(resp.content);
+  return JSON.parse(resp.content).results;
 }
