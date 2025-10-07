833f7fc167dd59c444985c9effc36468747b044b
Gabriel Pelouze <gabriel@pelouze.net>
2025-10-07 09:51:38 +0200

    add stories

---
+++ src/components/chart/FlowChart.stories.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+import { mapValues } from 'lodash';
+import {
+  actions,
+  FlowChart,
+  IChart,
+  INodeDefaultProps
+} from '@mrblenny/react-flow-chart';
+
+import { chart as mockChart } from '../../mocks/chart';
+import { NodeCustom } from './NodeCustom';
+import { NodeInnerCustom } from './NodeInnerCustom';
+import { PortCustom } from './PortCustom';
+
+function FlowChartStory() {
+  const [chart, setChart] = useState<IChart>(mockChart);
+  const chartStateActions = mapValues(
+    actions,
+    (func: any) =>
+      (...args: any) => {
+        const newChartTransformer = func(...args);
+        const newChart = newChartTransformer(chart);
+        setChart(chart => ({ ...chart, ...newChart }));
+      }
+  ) as typeof actions;
+  return (
+    <FlowChart
+      chart={chart}
+      callbacks={chartStateActions}
+      config={{
+        readonly: false
+      }}
+      Components={{
+        Node: NodeCustom as React.FunctionComponent<INodeDefaultProps>,
+        NodeInner: NodeInnerCustom,
+        Port: PortCustom
+      }}
+    />
+  );
+}
+
+const meta = {
+  component: FlowChartStory
+} satisfies Meta<typeof FlowChart>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
