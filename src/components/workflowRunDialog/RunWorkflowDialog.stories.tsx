5d14c035cb37ad412dcf8b95cd323cea3f1e3dae
Gabriel Pelouze <gabriel@pelouze.net>
2025-11-18 13:19:57 +0100

    add Jupyter Lab dialog around RunWorkflowDialog story

--- src/components/workflowRunDialog/RunWorkflowDialog.stories.tsx
+++ src/components/workflowRunDialog/RunWorkflowDialog.stories.tsx
@@ -2,6 +2,9 @@ import type { Meta, StoryObj } from '@storybook/react-webpack5';
 
 import { chart as mockChart } from '../../mocks/chart';
 import { RunWorkflowDialog } from './RunWorkflowDialog';
+import React from 'react';
+import '@jupyterlab/apputils/style/dialog.css';
+import '@jupyterlab/theme-light-extension/style/variables.css';
 
 const meta = {
   component: RunWorkflowDialog
@@ -14,5 +17,19 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     chart: mockChart
-  }
+  },
+  decorators: [
+    (Story, { parameters }) => {
+      return (
+        <div className="lm-widget jp-Dialog jp-ThemedContainer">
+          <div className="lm-widget jp-Dialog-content">
+            <div className="lm-widget jp-Dialog-header">Run Workflow</div>
+            <div className="lm-widget jp-Dialog-body">
+              <Story />
+            </div>
+          </div>
+        </div>
+      );
+    }
+  ]
 };
