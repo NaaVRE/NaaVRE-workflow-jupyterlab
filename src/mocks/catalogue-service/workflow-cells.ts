50164841566b348824817847339855b3d024f4c6
Gabriel Pelouze <gabriel@pelouze.net>
2026-01-19 15:22:13 +0100

    add s3 for catalogue in dev services and bump versions

--- src/mocks/catalogue-service/workflow-cells.ts
+++ src/mocks/catalogue-service/workflow-cells.ts
@@ -3,7 +3,7 @@ import { INaaVREExternalServiceResponse } from '../../naavre-common/handler';
 
 export const cells: ICell[] = [
   {
-    url: 'http://localhost:56848/workflow-cells/cbd0bc89-7418-4536-a8c4-6eb3bb6bf2e6/',
+    url: 'http://localhost:8000/workflow-cells/cbd0bc89-7418-4536-a8c4-6eb3bb6bf2e6/',
     owner: 'fixture-user-1',
     virtual_lab: 'test-virtual-lab-1',
     shared_with_scopes: ['test-community-1'],
@@ -29,7 +29,7 @@ export const cells: ICell[] = [
     next_version: null
   },
   {
-    url: 'http://localhost:56848/workflow-cells/d7fa87ca-79a3-43eb-993a-83afec93f1e4/',
+    url: 'http://localhost:8000/workflow-cells/d7fa87ca-79a3-43eb-993a-83afec93f1e4/',
     owner: 'fixture-user-1',
     virtual_lab: 'test-virtual-lab-1',
     shared_with_scopes: ['test-virtual-lab-1'],
@@ -55,7 +55,7 @@ export const cells: ICell[] = [
     next_version: null
   },
   {
-    url: 'http://localhost:56848/workflow-cells/77e49ffb-7688-40c5-8287-6693ae3d096a/',
+    url: 'http://localhost:8000/workflow-cells/77e49ffb-7688-40c5-8287-6693ae3d096a/',
     owner: 'fixture-user-1',
     virtual_lab: 'test-virtual-lab-2',
     shared_with_scopes: ['test-virtual-lab-2'],
@@ -81,7 +81,7 @@ export const cells: ICell[] = [
     next_version: null
   },
   {
-    url: 'http://localhost:56848/workflow-cells/a9ad6181-e78c-45b0-83c1-69385b9f9404/',
+    url: 'http://localhost:8000/workflow-cells/a9ad6181-e78c-45b0-83c1-69385b9f9404/',
     owner: 'fixture-user-1',
     virtual_lab: 'test-virtual-lab-1',
     shared_with_scopes: [],
@@ -107,7 +107,7 @@ export const cells: ICell[] = [
     next_version: null
   },
   {
-    url: 'http://localhost:56848/workflow-cells/3995b067-95fd-4706-9325-2b89339afb2c/',
+    url: 'http://localhost:8000/workflow-cells/3995b067-95fd-4706-9325-2b89339afb2c/',
     owner: 'test-user-2',
     virtual_lab: 'test-virtual-lab-2',
     shared_with_scopes: [],
@@ -165,7 +165,7 @@ export const cells: ICell[] = [
     next_version: null
   },
   {
-    url: 'http://localhost:56848/workflow-cells/b6923c9e-eeb6-49fe-81f2-3df7003f8332/',
+    url: 'http://localhost:8000/workflow-cells/b6923c9e-eeb6-49fe-81f2-3df7003f8332/',
     owner: 'test-user-2',
     virtual_lab: 'test-virtual-lab-1',
     shared_with_scopes: [],
@@ -197,7 +197,7 @@ export const cells: ICell[] = [
     next_version: null
   },
   {
-    url: 'http://localhost:56848/workflow-cells/d1d41322-1101-489c-82a1-8038ea999416/',
+    url: 'http://localhost:8000/workflow-cells/d1d41322-1101-489c-82a1-8038ea999416/',
     owner: 'test-user-2',
     virtual_lab: 'test-virtual-lab-1',
     shared_with_scopes: [],
@@ -253,10 +253,10 @@ export const cells: ICell[] = [
     kernel: 'ipython',
     source_url: '',
     next_version:
-      'http://localhost:56848/workflow-cells/b58c627a-1843-421c-a897-89461ddc581a/'
+      'http://localhost:8000/workflow-cells/b58c627a-1843-421c-a897-89461ddc581a/'
   },
   {
-    url: 'http://localhost:56848/workflow-cells/b58c627a-1843-421c-a897-89461ddc581a/',
+    url: 'http://localhost:8000/workflow-cells/b58c627a-1843-421c-a897-89461ddc581a/',
     owner: 'test-user-2',
     virtual_lab: 'test-virtual-lab-1',
     shared_with_scopes: [],
