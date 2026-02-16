476833a536afd321132cdceb93f12fbd7263990d
Gabriel Pelouze <gabriel@pelouze.net>
2026-02-16 11:20:47 +0100

    fix Composer story

--- src/components/Composer.stories.tsx
+++ src/components/Composer.stories.tsx
@@ -1,9 +1,24 @@
+import React, { useEffect, useRef } from 'react';
 import type { Meta, StoryObj } from '@storybook/react-webpack5';
 
 import { Composer } from './Composer';
+import { chart as mockChart } from '../mocks/chart';
+
+function ComposerStory() {
+  const composerRef = useRef<any>(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      composerRef.current?.setState({ chart: mockChart });
+    }, 100);
+
+    return () => clearTimeout(timer);
+  }, []);
+  return <Composer ref={composerRef} />;
+}
 
 const meta = {
-  component: Composer
+  component: ComposerStory
 } satisfies Meta<typeof Composer>;
 
 export default meta;
