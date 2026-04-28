9e69fd49a1ab83487ce167af7c9216b924e7296d
Gabriel Pelouze <gabriel@pelouze.net>
2026-04-28 14:40:43 +0200

    limit cell list filters drop down height to not hide button

--- src/mocks/catalogue-service/sharing-scopes.ts
+++ src/mocks/catalogue-service/sharing-scopes.ts
@@ -25,11 +25,19 @@ export const sharingScopes: ISharingScope[] = [
   },
   {
     url: 'http://localhost:8000/sharing-scopes/test-virtual-lab-2/',
-    title: 'Test Virtual Lab 2',
+    title:
+      'Test Virtual Lab 2 with a title just long enough to make you wonder whether it is reasonable',
     label: 'virtual_lab',
     show_in_virtual_labs: ['test-virtual-lab-1', 'test-virtual-lab-2'],
     check_in_virtual_labs: ['test-virtual-lab-2']
-  }
+  },
+  ...[...Array(20).keys()].map(i => ({
+    url: `http://localhost:8000/sharing-scopes/extra-virtual-lab-${i + 1}/`,
+    title: `Extra Virtual Lab ${i + 1}`,
+    label: 'virtual_lab' as const,
+    show_in_virtual_labs: ['test-virtual-lab-1', 'test-virtual-lab-2'],
+    check_in_virtual_labs: []
+  }))
 ];
 
 export async function getSharingScopesList(
